2ad12e908702b10da4aa4a698d2b7c71211ecd01
GABRIELLY VIEIRA DOS SANTOS <gabrielly.vsantos2@senacsp.edu.br>
2019-06-06 20:01:50 -0300

    OFICIAL

---
+++ js/form-validation.js
@@ -0,0 +1,12 @@
+<script language="javascript" type="text/javacript">
+
+
+    function validacao() {
+        var nome = matricula.Nome.value;
+
+}
+
+if (nome == "") {
+        alert(' Prencha o campo' + nome);
+    }
+</script>
